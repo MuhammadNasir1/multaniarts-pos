12d5f8c92f2d1d93d10f4c4c5a29bfcfde48734b
M-ArhamWaheed <info.epage.org@gmail.com>
2024-12-05 14:13:49 +0500

    Add Products in cutting

--- cutting.php
+++ cutting.php
@@ -143,10 +143,10 @@
                                                 <select class="form-control searchableSelect" name="type[]" id="type<?= $i ?>">
                                                     <option disabled selected>Select Type</option>
                                                     <?php
-                                                    $products = mysqli_query($dbc, "SELECT * FROM product WHERE brand_id = 'cutted' AND status = 1");
+                                                    $products = mysqli_query($dbc, "SELECT * FROM product WHERE brand_id = 'cora_cutted' OR brand_id = 'dyed_cutted' AND status = 1");
                                                     while ($p = mysqli_fetch_assoc($products)) {
                                                     ?>
-                                                        <option value="<?= $p['product_id'] ?>"><?= ucwords($p['product_name']) ?></option>
+                                                        <option value="<?= $p['product_id'] ?>"><?= ucwords($p['product_name']) ?> (<?= ucwords($p['brand_id']) ?>)</option>
                                                     <?php } ?>
                                                 </select>
                                             </div>
